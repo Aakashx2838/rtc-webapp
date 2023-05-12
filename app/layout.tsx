97daea99a2e3191baec0b495602b70c0190b3574
Aakashx2838 <meaakash000@gmail.com>
2023-05-12 09:23:25 +0545

    Removed Progressbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
         <AuthContext>
-          <LoadingProgressbar />
+          {/* <LoadingProgressbar /> */}
           <ToasterContext />
           <ActiveStatus />
           {children}
